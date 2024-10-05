44512525565a7198c1c78d79e0d2ad4670f2e2b9
PandyaDeval <dev23199@yahoo.in>
2024-10-04 17:49:09 -0700

    final changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,25 @@
 module.exports = eleventyConfig => {
 
-    const markdownIt = require('markdown-it')
-    const markdownItAttrs = require('markdown-it-attrs')
-  
-    const markdownItOptions = {
-      html: true,
-      breaks: true,
-      linkify: true
-    }
-  
-    const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
-  
-    eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.setLibrary('md', markdownLib);
-    eleventyConfig.markdownTemplateEngine = 'njk';
-  
-    return {
-      dir: {
-        input: "src",
-        output: "docs"
-      }
+  const markdownIt = require('markdown-it')
+  const markdownItAttrs = require('markdown-it-attrs')
+
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true
+  }
+
+  const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.setLibrary('md', markdownLib);
+  eleventyConfig.markdownTemplateEngine = 'njk';
+
+  return {
+    pathPrefix: '/publishing-11ty/',
+    dir: {
+      input: "src",
+      output: "docs"
     }
   }
+}
